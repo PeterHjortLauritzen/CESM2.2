ede58fac2315c9986bf0b310f243aad1296ee8aa
PeterHjortLauritzen <pel@ucar.edu>
2020-06-02 16:31:15 -0600

    add scripts

---
+++ data/run-scripts/performance_master.sh
@@ -0,0 +1,8 @@
+#!/bin/csh
+#set res="ne30pg3_ne30pg3_mg17"      #cslam
+set res="ne30_ne30_mg17"
+foreach pecount (450 900 1800 2700 5400)
+  source /glade/u/home/pel/git-scripts/run-cam/collection/performance.sh
+end
+
+
